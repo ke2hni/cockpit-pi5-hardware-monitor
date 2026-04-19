bb9c310f315104e80f8242de5e1ea9d19a8baeca
ke2hni <ke2hni@gmail.com>
2026-04-18 21:37:22 -0400

    Update version #

--- src/app.tsx
+++ src/app.tsx
@@ -1658,7 +1658,7 @@ export const Application = () => {
                             <FlexItem flex={{ default: "flex_1" }}>
                                 <Title headingLevel="h1">Raspberry Pi 5 Hardware Monitor</Title>
                                 <Content component={ContentVariants.p}>
-                                    Ver. 1.2 - April 2026
+                                    Ver. 2.0 - April 18, 2026
                                 </Content>
                             </FlexItem>
 
